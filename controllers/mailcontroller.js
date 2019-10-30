f5585fb06be8dd56e75a812050942197934f8ce5
varunreddy624 <varunreddy04022000@gmail.com>
2019-10-30 21:51:35 +0530

    commi11

--- controllers/mailcontroller.js
+++ controllers/mailcontroller.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var email = require('nodemailer');
 var node = require('node-schedule');
-
+var async = require("async");
 
 //Connect to the data base
 mongoose.connect("mongodb+srv://test:test1234@todo-qqfes.mongodb.net/test?retryWrites=true&w=majority",{ useUnifiedTopology: true, useNewUrlParser: true });
@@ -10,43 +10,35 @@ var pro = mongoose.model('signup');
 //create a schema (Blue print)
 module.exports = function(app){
   var sendEmail = node.scheduleJob('*/1 * * * *', function(){
+    var transporter = email.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'varunreddyforever@gmail.com',
+      pass: 'HAPPYUGADI@101'
+    }
+  });
+    var date = new Date();
+    const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
     pro.find({},function(err,data){
       if(err) throw err;
-      for(var j=0;j<data.length;j++)
-      {
-        if(data[j].role=='student')
+    async.each(data, function(student, callback){
+        if(student.role=='student')
         {
-          var date = new Date();
+          console.log(student);
           var text="";
-          const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
-          for(var i=0;i<data[j].task.length;i++)
-          {
-            if(data[j].task[i].date>date)
-              text=text+ data[j].task[i].item + '  -  ' + data[j].task[i].date.getDate() + "th of " + monthNames[data[j].task[i].date.getMonth()] + "," + data[j].task[i].date.getFullYear() + '\n';
-          }
-          var transporter = email.createTransport({
-          service: 'gmail',
-          auth: {
-            user: 'varunreddyforever@gmail.com',
-            pass: 'HAPPYUGADI@101'
-          }
+          async.each(student.task,function(a,callback){
+            if(a.date>date)
+              text=text+ a.item + '  -  ' + a.date.getDate() + "th of " + monthNames[a.date.getMonth()] + "," + a.date.getFullYear() + '\n';
           });
           var mailOptions = {
             from: 'varunreddyforever@gmail.com',
-            to: data[j].emailid,
+            to: student.emailid,
             subject: 'Super procrastinator',
-            text: "following tasks are incomplete\n"+text+"\n"+ "for more info visit our website here\n http://127.0.0.1:3000/pro"
+            text: "following tasks are incomplete\n"+text+"\n"+ "for more info visit our website here\n http://127.0.0.1:3000/"
           };
-
-          transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-              console.log(error);
-            } else {
-              console.log('Email sent: to'+data[j].emailid);
-            }
-          });
+          transporter.sendMail(mailOptions);
         }
-      }
+      });
     });
   });
 };
